82adbeb74e0159649586d135efb91f9e37a9ead3
Showviq <showviq@iut-dhaka.edu>
2023-03-11 20:03:42 +0600

    Mail Service Test

---
+++ SimpleEmailApp/Controllers/EmailController.cs
@@ -0,0 +1,36 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MimeKit.Text;
+
+namespace SimpleEmailApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult SendEmail(string body)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("alanna.runte50@ethereal.email"));
+            email.To.Add(MailboxAddress.Parse("alanna.runte50@ethereal.email"));
+            email.Subject = "Test Email Subject";
+            email.Body = new TextPart(TextFormat.Html) { Text = body };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate("alanna.runte50@ethereal.email", "6Qh5bKqpGWcx8FzBWB");
+            smtp.Send(email);
+            smtp.Disconnect(true);
+
+            return Ok();
+        }
+    
+    
+    }
+
+}
